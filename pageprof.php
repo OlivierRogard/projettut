9ead171f422344180a3df9abe39ad18ec736cf45
CoralieRT <34239901+CoralieRT@users.noreply.github.com>
2018-11-29 17:05:31 +0100

    Update pageprof.php

--- pageprof.php
+++ pageprof.php
@@ -15,92 +15,89 @@
         <header> IUT de Saint-Malo </header>
         <h1>Feuille d'absence</h1>
         <h4><a href="trombi.php">Trombi des étudiants manquants</a></h4>
-            <h4><a href="qrcode.php?c=<?php if(isset($_SESSION['id_cours'])){ echo $_SESSION['id_cours'];}?>" target="_blank">Lien QR </a></h4>
+        <h4><a href="qrcode.php?c=<?php if(isset($_SESSION['id_cours'])){ echo $_SESSION['id_cours'];}?>" target="_blank">Lien QR </a></h4>
 
-                <?php
+            <?php
 
-                if(isset($_SESSION['id_cours'])){
-                    $cours = $_SESSION['id_cours'];  
-                    /*traitement de la chaîne de caractère, identification promo*/
-                    $rep=$bdd->prepare('SELECT `id_promo` FROM `cours` WHERE `id_cours` = ?');
-                    $rep->execute(array($cours));
-                    while($res=$rep->fetch()){
-                        $promo=$res['id_promo'];
-                        /*cours en groupe*/
-                        if(strlen($promo)==5){
-                            $gr=substr($promo,-1);
-                            $_SESSION['gr']=$gr;
-                            $promo=substr($promo,0,-2).'A';   
-                            /*affichage des étudiants en fonction de leur promo et leur groupe*/
-                            $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe= ? AND id_promo = ? ORDER BY Nom');
-                            $reponse->execute(array($gr,$promo));  
-                        }
-                        /*cours en classe entière*/
-                        else if(substr($promo,0,3)=='R&T'){
-                            $promo=substr($promo,-4);
-                            $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE id_promo = ? ORDER BY Nom');
-                            $reponse->execute(array($promo));
-                        }
-                        $_SESSION['id_promo']=$promo;
+            if(isset($_SESSION['id_cours'])){
+                $cours = $_SESSION['id_cours'];
+                echo $cours;
+                /*traitement de la chaîne de caractère, identification promo*/
+                $rep=$bdd->prepare('SELECT `id_promo` FROM `cours` WHERE `id_cours` = ?');
+                $rep->execute(array($cours));
+                while($res=$rep->fetch()){
+                    $promo=$res['id_promo'];
+                    /*cours en groupe*/
+                    if(strlen($promo)==5){
+                        $gr=substr($promo,-1);
+                        //$_SESSION['gr']=$gr;
+                        $nvpromo=substr($promo,0,-2).'A';
+                        /*affichage des étudiants en fonction de leur promo et leur groupe*/
+                        $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE Groupe = ? AND id_promo = ? ORDER BY Nom');
+                        $reponse->execute(array($gr,$nvpromo)); 
+                    } 
+                    if(strlen($promo)==4){
+                        $reponse = $bdd->prepare('SELECT * FROM bdd_promo.etudiant WHERE id_promo = ? ORDER BY Nom');
+                        $reponse->execute(array($promo));
                     }
-                ?>
-            <table id="b">
-                <thead>
-                    <tr>
-                        <td colspan="3"="3">Promotion</td>
-                    </tr>
-                    <tr>
-                        <td colspan="3"><?php echo $promo; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Prénom</td>
-                        <td>Photo</td>
-                    </tr>
-                </thead>
-                <tbody>
+                    $_SESSION['id_promo']=$promo;
+                }
+            ?>
+        <table id="b">
+            <thead>
                 <tr>
-                    <td colspan="3" ="3"><?php if(isset($gr)){ echo 'Groupe'.$gr;} else{ echo " Classe entière";}?></td>
+                    <td colspan="3"="3">Promotion</td>
                 </tr>
-                <?php
-                while ($res = $reponse->fetch())                                         //On affiche une nouvelle ligne par étudiant trouvé
-                {
-
+                <tr>
+                    <td colspan="3"><?php echo $promo; ?></td>
+                </tr>
+                <tr>
+                    <td>Nom</td>
+                    <td>Prénom</td>
+                    <td>Photo</td>
+                </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td colspan="3" ="3"><?php if(isset($gr)){ echo 'Groupe'.$gr;} else{ echo " Classe entière";}?></td>
+            </tr>
+            <?php
+            while ($res = $reponse->fetch())                                         //On affiche une nouvelle ligne par étudiant trouvé
+            {
                 $abs = $bdd->prepare('SELECT * FROM bdd_promo.etudiant,bdd_promo.absences WHERE etudiant.login=? AND etudiant.login=absences.loginetu');
                 $abs->execute(array($res['login']));
                 $jtot=0;
                 $njtot=0;
                 $atot=0;
-
+            ?>
+            <tr>
+                <td><?php echo $res['Nom']; 
+                    while($rep=$abs->fetch())
+                    {
+                        $njtot=$njtot+$rep['nj'];
+                        $jtot=$jtot+$rep['j'];
+                        $atot=$atot+1;
+                    }
                 ?>
-                <tr>
-                    <td><?php echo $res['Nom']; 
-                        while($rep=$abs->fetch())
-                        {
-                            $njtot=$njtot+$rep['nj'];
-                            $jtot=$jtot+$rep['j'];
-                            $atot=$atot+1;
-                        }
-                    ?>
-                        <li>
-                            <ul>nombre d'absences justifiées : <?php echo $jtot; ?></ul>
-                            <ul>nombre d'absences injustifiées : <?php echo $njtot; ?></ul>
-                            <ul>absences totales : <?php echo $atot; ?></ul>
-                        </li>
-                    </td>
-                    <td><?php echo $res['Prénom']; ?></td>
-                    <td><img src="trombi/<?php echo $res['photo']; ?>.png" title="trombi"/></td>
-                </tr>
-                <?php
-                }
-                $reponse->closeCursor();                                            // Termine le traitement de la requête
+                    <li>
+                        <ul>nombre d'absences justifiées : <?php echo $jtot; ?></ul>
+                        <ul>nombre d'absences injustifiées : <?php echo $njtot; ?></ul>
+                        <ul>absences totales : <?php echo $atot; ?></ul>
+                    </li>
+                </td>
+                <td><?php echo $res['Prénom']; ?></td>
+                <td><img src="trombi/<?php echo $res['photo']; ?>.png" title="trombi"/></td>
+            </tr>
+            <?php
             }
-            else {
-                echo "Vous n'avez pas cours en ce moment ! :)";
-            }
-                ?>
-            </tbody>
-        </table>
+            $reponse->closeCursor();                                            // Termine le traitement de la requête
+        }
+        else {
+            echo "Vous n'avez pas cours en ce moment ! :)";
+        }
+            ?>
+        </tbody>
+    </table>
         <script>
                 function Deconnexion ()
                 {
